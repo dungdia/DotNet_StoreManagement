b39b86356a7b2b9194cead316ae3ea7c8f54bf14
HMean <HMean0309@gmail.com>
2025-10-17 23:32:42 +0700

    them Payment API

---
+++ Features/PaymentAPI/PaymentRepository.cs
@@ -0,0 +1,14 @@
+﻿using DotNet_StoreManagement.Domain.entities;
+using DotNet_StoreManagement.Features.PaymentAPI.impl;
+using DotNet_StoreManagement.SharedKernel.configuration;
+using DotNet_StoreManagement.SharedKernel.persistence;
+
+namespace DotNet_StoreManagement.Features.PaymentAPI;
+
+[Repository]
+public class PaymentRepository : DPARepository<Payment, int>, IPaymentRepository
+{
+    public PaymentRepository(AppDbContext context) : base(context)
+    {
+    }
+}
---
+++ Features/PaymentAPI/dtos/PaymentDTO.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DotNet_StoreManagement.Domain.entities;
+
+
+namespace DotNet_StoreManagement.Features.PaymentAPI.dtos;
+
+[AutoMap(typeof(Payment), ReverseMap = true)]
+public class PaymentDTO
+{
+
+    public decimal Amount { get; set; }
+    
+    public string? PaymentMethod { get; set; }
+
+    public DateTime? PaymentDate { get; set; }
+}
