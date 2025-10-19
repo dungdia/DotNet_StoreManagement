e3a732c45b4e13e2f5c0ee3ae6f6c0cdda8c1d82
Hai Dun <duonghuynh22092004@gmail.com>
2025-10-19 16:20:22 +0700

    Init Interface Repo + DTO

---
+++ Features/InventoryAPI/dtos/InventoryDTO.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using DotNet_StoreManagement.Domain.entities;
+
+namespace DotNet_StoreManagement.Features.InventoryAPI;
+
+[AutoMap(typeof(Inventory), ReverseMap = true)]
+public class InventoryDTO
+{
+    public int InventoryId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ Features/InventoryAPI/impl/IInventoryRepository.cs
@@ -0,0 +1,10 @@
+using DotNet_StoreManagement.Domain.entities;
+using DotNet_StoreManagement.Domain.entities.@base;
+using DotNet_StoreManagement.SharedKernel.persistence;
+using DotNet_StoreManagement.SharedKernel.persistence.impl;
+
+namespace DotNet_StoreManagement.Features.InventoryAPI.impl;
+
+public interface IInventoryRepository : IDPARepository<Inventory, int>
+{
+}
