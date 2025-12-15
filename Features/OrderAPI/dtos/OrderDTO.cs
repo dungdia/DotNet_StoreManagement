a8d39096de9193ff026c6ec561040df15c8da3bc
Duc3m <ducemtran44@gmail.com>
2025-12-15 10:53:01 +0700

    feat: repay order

--- Features/OrderAPI/dtos/OrderDTO.cs
+++ Features/OrderAPI/dtos/OrderDTO.cs
@@ -12,4 +12,6 @@ public class OrderDTO
     public int PromoId { get; set; }
     public decimal TotalAmount { get; set; }
     public decimal DiscountAmount { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string? Status { get; set; }
 }
